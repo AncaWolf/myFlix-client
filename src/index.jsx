6d932f43f8309c8f093a62b3b2bd85c28074d78b
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-11-10 20:49:40 +0100

    imported MainView

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,27 @@
 import { createRoot } from 'react-dom/client';
+import { MainView } from './components/main-view/main-view';
 
 // statement to indicated that you need to bundle `./index.scss`
 import "./index.scss";
 
 // main component (will eventually use all the others)
-const MyFlixApplication = () => {
+// const MyFlixApplication = () => {
+//     return (
+//         <div className="my-flix">
+//             <div>Good morning</div>
+//         </div>
+//     );
+// };
+
+const MovieApp = () => {
     return (
-        <div className="my-flix">
-            <div>Good morning</div>
-        </div>
-    );
-};
+        <MainView />
+    )
+}
 
 // needed to find the root of the app
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
 // this tells React to render the app in the root DOM element
-root.render(<MyFlixApplication />);
+root.render(<MovieApp />);
