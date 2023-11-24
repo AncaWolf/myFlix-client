ad44d7832f145d55140c63ce47ed671a7d3baa83
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-11-24 19:34:39 +0100

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,67 +2,56 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 export const LoginView = ({ onLoggedIn }) => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [token, setToken] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState(null);
 
-    if (!user) {
-        return (
-            <LoginView onLoggedIn={(user, token) => {
-                setUser(user);
-                setToken(token);
-            }}
-            />
-        );
-    }
-
-    if (data.user) {
-        localStorage.setItem("user", JSON.stringify(data.user));
-        localStorage.setItem("token", data.token);
-        onLoggedIn(data.user, data.token);
-    } else {
-        alert("No such user");
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = {
-            access: username,
-            secret: password
-        };
-        fetch("", {
-            method: "POST",
-            body: JSON.stringify(data)
-        }).then((response) => {
-            if (response.ok) {
-                onLoggedIn(username);
-            } else {
-                alert("Login failed");
-            }
-        });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      access: username,
+      secret: password,
     };
+    fetch("https://awolf-movies-app.onrender.com", {
+      method: "POST",
+      body: JSON.stringify(data),
+      header: {
+        "Content-Type": "application/json"
+      }
+    }).then((response) => response.json())
+      .then((data) => {
+        console.log("login response: ", data);
+        if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("No such user");
+        }
+      })
+  }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username:
-                <input
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
-            </label>
-            <label>
-                Password:
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
