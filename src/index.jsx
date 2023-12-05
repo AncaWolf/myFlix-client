43a557645399732604f9c02a816042cdf1500d3a
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-12-05 19:38:18 +0100

    added container to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
+import Container from "react-bootstrap/Container";
+import "./index.scss";
 
 // statement to indicated that you need to bundle `./index.scss`
 import "./index.scss";
@@ -15,9 +17,11 @@ import "./index.scss";
 
 const MovieApp = () => {
     return (
-        <MainView />
-    )
-}
+        <Container>
+            <MainView />
+        </Container>
+    );
+};
 
 // needed to find the root of the app
 const container = document.querySelector("#root");
