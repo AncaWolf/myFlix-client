2a1d4c5eab019454caaea9f6aefc10bd755189b3
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-12-19 21:43:59 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -17,24 +17,9 @@ export const MovieCard = ({ movie }) => {
   );
 };
 
-// export const MovieCard = ({ movie, onMovieClick }) => {
-//   return (
-//     <Card className="h-100">
-//       <Card.Img className="w-100" variant="top" src={movie.ImagePath} />
-//       <Card.Body>
-//         <Card.Title>{movie.Title}</Card.Title>
-//         <Button onClick={() => onMovieClick(movie)} variant="link">
-//           View
-//         </Button>
-//       </Card.Body>
-//     </Card>
-//   );
-// };
-
 // defining PropTypes constraints
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
   }).isRequired,
-  // onMovieClick: PropTypes.func.isRequired,
 };
