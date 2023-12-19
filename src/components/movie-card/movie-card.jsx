d9ffd60d9f054a1076d8c5785e31ece263a19a39
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-12-19 20:56:36 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,32 +1,40 @@
 import PropTypes from "prop-types";
 import "./movie-card.scss";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
     <Card className="h-100">
       <Card.Img className="w-100" variant="top" src={movie.ImagePath} />
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
-        {/* <Card.Text>{movie.Director.Name}</Card.Text> */}
-        <Button onClick={() => onMovieClick(movie)} variant="link">
-          View
-        </Button>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+          <Button variant="link">View</Button>
+        </Link>
       </Card.Body>
     </Card>
-    // <div
-    //   onClick={() => {
-    //     onMovieClick(movie);
-    //   }}>
-    //   {movie.title}
-    // </div>
   );
 };
+
+// export const MovieCard = ({ movie, onMovieClick }) => {
+//   return (
+//     <Card className="h-100">
+//       <Card.Img className="w-100" variant="top" src={movie.ImagePath} />
+//       <Card.Body>
+//         <Card.Title>{movie.Title}</Card.Title>
+//         <Button onClick={() => onMovieClick(movie)} variant="link">
+//           View
+//         </Button>
+//       </Card.Body>
+//     </Card>
+//   );
+// };
 
 // defining PropTypes constraints
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
+  // onMovieClick: PropTypes.func.isRequired,
 };
