8d4006d6d847324d35f145b376f67f4d9c2c3a13
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-12-21 21:55:13 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,16 +2,84 @@ import PropTypes from "prop-types";
 import "./movie-card.scss";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-export const MovieCard = ({ movie }) => {
+export const MovieCard = ({ movie, token, setUser, user }) => {
+  const [isFavourite, setIsFavourite] = useState(
+    false
+  );
+
+  useEffect(() => {
+    if (user.FavouriteMovies && user.FavouriteMovies.includes(movie.id)) {
+      setIsFavourite(true);
+    }
+  }, [user]);
+
+  const addFavouriteMovie = () => {
+    fetch(
+      `https://awolf-movies-app.onrender.com/users/${user.username}/movies/${movie.id}`,
+      { method: "POST", headers: { Authorization: `Bearer ${token}` } }
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.log("Failed to add movie to favourites");
+        }
+      })
+      .then((user) => {
+        if (user) {
+          alert("Movie successfully added to favorites");
+          localStorage.setItem("user", JSON.stringify(user));
+          setUser(user);
+          setIsFavourite(true);
+        }
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
+  const removeFavouriteMovie = () => {
+    fetch(
+      `https://awolf-movies-app.onrender.com/users/${user.username}/movies/${movie.id}`,
+      { method: "DELETE", headers: { Authorization: `Bearer ${token}` } }
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          alert("Operation failed");
+        }
+      })
+      .then((user) => {
+        if (user) {
+          alert("Movie was successfully deleted from favorites");
+          localStorage.setItem("user", JSON.stringify(user));
+          setUser(user);
+          setIsFavourite(false);
+        }
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
   return (
     <Card className="h-100">
       <Card.Img className="w-100" variant="top" src={movie.ImagePath} />
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
         <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
-          <Button variant="link">View</Button>
+          <Button variant="link">See movie details</Button>
         </Link>
+        <Card.Body className="favorite-btns">
+          {!isFavourite ? (
+            <Button className="fav-btn" onClick={addFavouriteMovie}>+</Button>
+          ) : (
+            <Button className="fav-btn" onClick={removeFavouriteMovie}>-</Button>
+          )}
+        </Card.Body>
       </Card.Body>
     </Card>
   );
@@ -20,6 +88,6 @@ export const MovieCard = ({ movie }) => {
 // defining PropTypes constraints
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
+    Title: PropTypes.string,
   }).isRequired,
 };
