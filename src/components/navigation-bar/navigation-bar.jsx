8f9d5c7688ca6544ebe64e44f49f7f1f48942df4
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2024-01-24 20:44:44 +0100

    Update navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,40 +1,51 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { MovieFilter, MoviesFilter } from "../movies-filter/movies-filter";
+import { useSelector, useDispatch } from "react-redux";
+import { setUser } from "../../redux/reducers/user";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
-    return (
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand as={Link} to="/">
-                    myFlix App
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        {!user && (
-                            <>
-                                <Nav.Link as={Link} to="/login">
-                                    Login
-                                </Nav.Link>
-                                <Nav.Link as={Link} to="/signup">
-                                    Signup
-                                </Nav.Link>
-                            </>
-                        )}
-                        {user && (
-                            <>
-                                <Nav.Link as={Link} to="/">
-                                    Home
-                                </Nav.Link>
-                                <Nav.Link as={Link} to="/profile">
-                                    Profile
-                                </Nav.Link>
-                                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
-                            </>
-                        )}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          myFlix App
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {!user && (
+              <>
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/signup">
+                  Signup
+                </Nav.Link>
+              </>
+            )}
+            {user && (
+              <>
+                <Nav.Link as={Link} to="/">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/profile">
+                  Profile
+                </Nav.Link>
+                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+                <Nav.Link onClick={() => dispatch(setUser(null))}>
+                  Logout
+                </Nav.Link>
+
+              </>
+            )}
+          </Nav>
+          <Nav>{isMainView && <MoviesFilter />}</Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
