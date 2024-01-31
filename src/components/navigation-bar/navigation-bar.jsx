a9c093a97c5a5af9040ff48fc3c47813c214dfde
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2024-01-31 15:15:07 +0100

    Update navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,11 +1,10 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { MovieFilter, MoviesFilter } from "../movies-filter/movies-filter";
 import { useSelector, useDispatch } from "react-redux";
 import { setUser } from "../../redux/reducers/user";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
-  const { user } = useSelector((state) => state.user);
+  const currentUser = useSelector((state) => state.user); // Use a different variable name to avoid conflicts
   const dispatch = useDispatch();
 
   return (
@@ -17,7 +16,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            {!user && (
+            {!currentUser.user && ( // Use currentUser.user instead of user to check if the user is logged in
               <>
                 <Nav.Link as={Link} to="/login">
                   Login
@@ -27,7 +26,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                 </Nav.Link>
               </>
             )}
-            {user && (
+            {currentUser.user && (
               <>
                 <Nav.Link as={Link} to="/">
                   Home
@@ -39,13 +38,65 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                 <Nav.Link onClick={() => dispatch(setUser(null))}>
                   Logout
                 </Nav.Link>
-
               </>
             )}
           </Nav>
-          <Nav>{isMainView && <MoviesFilter />}</Nav>
+          {/* Use isMainView conditionally if needed
+          {isMainView && <MoviesFilter />}  */}
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 };
+
+// import { Navbar, Container, Nav } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import { MovieFilter, MoviesFilter } from "../movies-filter/movies-filter";
+// import { useSelector, useDispatch } from "react-redux";
+// import { setUser } from "../../redux/reducers/user";
+
+// export const NavigationBar = ({ user, onLoggedOut }) => {
+//   const { user } = useSelector((state) => state.user);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <Navbar bg="light" expand="lg">
+//       <Container>
+//         <Navbar.Brand as={Link} to="/">
+//           myFlix App
+//         </Navbar.Brand>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className="me-auto">
+//             {!user && (
+//               <>
+//                 <Nav.Link as={Link} to="/login">
+//                   Login
+//                 </Nav.Link>
+//                 <Nav.Link as={Link} to="/signup">
+//                   Signup
+//                 </Nav.Link>
+//               </>
+//             )}
+//             {user && (
+//               <>
+//                 <Nav.Link as={Link} to="/">
+//                   Home
+//                 </Nav.Link>
+//                 <Nav.Link as={Link} to="/profile">
+//                   Profile
+//                 </Nav.Link>
+//                 <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+//                 <Nav.Link onClick={() => dispatch(setUser(null))}>
+//                   Logout
+//                 </Nav.Link>
+
+//               </>
+//             )}
+//           </Nav>
+//           <Nav>{isMainView && <MoviesFilter />}</Nav>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// };
