2f7f698143780d0620a9114c436bedf5faed30c9
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2024-01-31 15:15:25 +0100

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "./reducers/movies";
-
+import userReducer from "./reducers/user";
 
 export const store = configureStore({
-  reducer: { movies: moviesReducer }
+  reducer: {
+    movies: moviesReducer,
+    user: userReducer,
+  },
 });
+
+export default store;
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import moviesReducer from "./reducers/movies";
+// import userReducer from "./reducers/users";
+
+
+// export const store = configureStore({
+//   reducer: { movies: moviesReducer }
+// });
 
 
