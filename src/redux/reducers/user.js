4efbc611366d0b79f83dc9366d459ba5cf86fb2b
Anca <anca.m.gozman@gmail.com>
2024-02-11 11:57:31 +0100

    update user.js

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,19 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const userSlice = createSlice({
-    name: "user",
-    initialState: { user: null, token: null },
-    reducers: {
-        setUser: (state, action) => {
-            state.user = action.payload.user;
-            state.token = action.payload.token;
-        },
-        clearUser: (state) => {
-            state.user = null;
-            state.token = null;
-            localStorage.clear();
-        },
+  name: "user",
+  initialState: { user: null, token: null },
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
     },
+    clearUser: (state) => {
+      state.user = null;
+      state.token = null;
+      localStorage.clear();
+    },
+  },
 });
 
 export const { setUser, clearUser } = userSlice.actions;
