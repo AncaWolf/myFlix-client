db8dc1623f29b00fdc181dfbc61f0200dc25096d
Anca <anca.m.gozman@gmail.com>
2024-02-21 21:30:29 +0100

    fixed filter and signup issues
    removed old commented code
    corrected movie.title - > movie.Title (for filter)
    removed condition at signup route

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -12,49 +12,18 @@ import "./main-view.scss";
 import { useSelector, useDispatch } from "react-redux";
 import { setMovies } from "../../redux/reducers/movies";
 import { setUser } from "../../redux/reducers/user";
-// import { MoviesList } from "../movies-list/movies-list";
-// import { MoviesFilter } from "../movies-filter/movies-filter";
-
-
-
-
 export const MainView = () => {
   const movies = useSelector((state) => state.movies.list);
   const { user, token } = useSelector((state) => state.user);
-
-  //NIzar
-  // let storedUser = null;
-  // try {
-  //   const userFromStorage = localStorage.getItem("user");
-  //   if (userFromStorage) {
-  //     storedUser = JSON.parse(userFromStorage);
-  //   }
-  // } catch (error) {
-  //   console.error("Error parsing user from localStorage:", error);
-  //   // Handle the error (e.g., clearing the invalid item or notifying the user)
-  // }
-
-  // lines above(25-34) - trying something else (in useEffect bellow)-Anca
-
-  //NIzar
-  // const storedToken = localStorage.getItem("token"); - moved in useEffect(Anca)
-  // const [user, setUser] = useState(storedUser ? storedUser : null);
-  // const [token, setToken] = useState(storedToken ? storedToken : null);
-  // const [movies, setMovies] = useState([]);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
 
   const filter = useSelector((state) => state.movies.filter).trim().toLowerCase();
-  // const filteredMovies = movies.filter((movie) => movie.title.toLowerCase().includes(filter));
-  const filteredMovies = movies; // Temporarily bypass the filter
-  console.log("Filtering movies with:", filter);
-
-
-  movies.forEach(movie => console.log(movie.title));
+  const filteredMovies = movies.filter((movie) => movie.Title && movie.Title.toLowerCase().includes(filter));
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
-    const storedToken = localStorage.getItem("token");
     if (storedUser && storedToken) {
       dispatch(setUser({ user: storedUser, token: storedToken }));
     }
@@ -70,9 +39,9 @@ export const MainView = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        setMovies(data);
+        dispatch(setMovies(data)); // Corrected line
       });
-  }, [token]);
+  }, [token, dispatch]);
 
   const addFav = (_id) => {
     fetch(`https://awolf-movies-app.onrender.com/users/${user.Username}/movies/${movie._id}`, {
@@ -123,11 +92,9 @@ export const MainView = () => {
       <NavigationBar
         user={user}
         onLoggedOut={() => {
-          setUser(null);
-          setToken(null);
-          localStorage.clear()
-          // localStorage.removeItem("token");
-          // localStorage.removeItem("user");
+          dispatch(setUser({ user: null, token: null }));
+          localStorage.removeItem("token");
+          localStorage.removeItem("user");
         }}
       />
       <Row className="main-container d-flex justify-content-center">
@@ -135,15 +102,9 @@ export const MainView = () => {
           <Route
             path="/signup"
             element={
-              <>
-                {!user ? (
-                  <Navigate to="/" />
-                ) : (
-                  <Col md={5}>
-                    <SignupView />
-                  </Col>
-                )}
-              </>
+              <Col md={5}>
+                <SignupView />
+              </Col>
             }
           />
           <Route
@@ -154,7 +115,7 @@ export const MainView = () => {
                   <Navigate to="/" />
                 ) : (
                   <Col md={5}>
-                    <LoginView /* onLoggedIn={(user) => setUser(user)} */ />
+                    <LoginView onLoggedIn={(user) => setUser(user)} />
                   </Col>
                 )}
               </>
@@ -166,15 +127,11 @@ export const MainView = () => {
               <>
                 {!user ? (
                   <Navigate to="/login" replace />
-                ) : movies.length === 0 ? (
-                  <Col>The list is empty!</Col>
                 ) : (
                   <Col md={8}>
-                    {/* <MovieView movies={movies} /> */}
-                    <MovieView user={user} token={token} setUser={setUser} />
+                    <MovieView movies={movies} />
                   </Col>
                 )}
-
               </>
             }
           />
@@ -182,22 +139,19 @@ export const MainView = () => {
             path="/"
             element={
               <>
-                {!user ?
-
-                  (
-                    <Navigate to="/login" replace />
-                  ) : movies.length === 0 ? (
-                    <Col>There are no movies in the list!</Col>
-                  ) : (
-                    <>
-                      {filteredMovies.map((movie) => (
-                        <Col className="mb-5" key={movie._id} md={3}>
-                          <MovieCard movie={movie} user={user} setUser={setUser} token={token} />
-                        </Col>
-                      ))}
-                    </>
-                  )
-                }
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>There are no movies in the list!</Col>
+                ) : (
+                  <>
+                    {filteredMovies.map((movie) => (
+                      <Col className="mb-5" key={movie._id} md={3}>
+                        <MovieCard movie={movie} user={user} setUser={setUser} token={token} />
+                      </Col>
+                    ))}
+                  </>
+                )}
               </>
             }
           />
@@ -228,8 +182,6 @@ export const MainView = () => {
 };
 
 
-
-
 // import { useEffect, useState } from "react";
 // import { MovieCard } from "../movie-card/movie-card";
 // import { MovieView } from "../movie-view/movie-view";
@@ -244,6 +196,8 @@ export const MainView = () => {
 // import { useSelector, useDispatch } from "react-redux";
 // import { setMovies } from "../../redux/reducers/movies";
 // import { setUser } from "../../redux/reducers/user";
+// // import { MoviesList } from "../movies-list/movies-list";
+// // import { MoviesFilter } from "../movies-filter/movies-filter";
 
 
 
@@ -251,18 +205,38 @@ export const MainView = () => {
 // export const MainView = () => {
 //   const movies = useSelector((state) => state.movies.list);
 //   const { user, token } = useSelector((state) => state.user);
-//   const storedUser = JSON.parse(localStorage.getItem("user"));
-//   const storedToken = localStorage.getItem("token");
+
+//   //NIzar
+//   // let storedUser = null;
+//   // try {
+//   //   const userFromStorage = localStorage.getItem("user");
+//   //   if (userFromStorage) {
+//   //     storedUser = JSON.parse(userFromStorage);
+//   //   }
+//   // } catch (error) {
+//   //   console.error("Error parsing user from localStorage:", error);
+//   //   // Handle the error (e.g., clearing the invalid item or notifying the user)
+//   // }
+
+//   // lines above(25-34) - trying something else (in useEffect bellow)-Anca
+
+//   //NIzar
+//   // const storedToken = localStorage.getItem("token"); - moved in useEffect(Anca)
 //   // const [user, setUser] = useState(storedUser ? storedUser : null);
 //   // const [token, setToken] = useState(storedToken ? storedToken : null);
 //   // const [movies, setMovies] = useState([]);
+
 //   const filter = useSelector((state) => state.movies.filter).trim().toLowerCase();
-//   // const filteredMovies = movies.filter((movie) => movie.title.toLowerCase().includes(filter));
-//   const filteredMovies = movies; // Temporarily bypass the filter
+//   const filteredMovies = movies.filter((movie) => movie.Title && movie.Title.toLowerCase().includes(filter));
+
+
+//   movies.forEach(movie => console.log(movie.Title));
 
 //   const dispatch = useDispatch();
 
 //   useEffect(() => {
+//     const storedUser = JSON.parse(localStorage.getItem("user"));
+//     const storedToken = localStorage.getItem("token");
 //     if (storedUser && storedToken) {
 //       dispatch(setUser({ user: storedUser, token: storedToken }));
 //     }
@@ -332,8 +306,10 @@ export const MainView = () => {
 //         user={user}
 //         onLoggedOut={() => {
 //           setUser(null);
-//           localStorage.removeItem("token");
-//           localStorage.removeItem("user");
+//           setToken(null);
+//           localStorage.clear()
+//           // localStorage.removeItem("token");
+//           // localStorage.removeItem("user");
 //         }}
 //       />
 //       <Row className="main-container d-flex justify-content-center">
@@ -341,15 +317,9 @@ export const MainView = () => {
 //           <Route
 //             path="/signup"
 //             element={
-//               <>
-//                 {!user ? (
-//                   <Navigate to="/" />
-//                 ) : (
-//                   <Col md={5}>
-//                     <SignupView />
-//                   </Col>
-//                 )}
-//               </>
+//               <Col md={5}>
+//                 <SignupView />
+//               </Col>
 //             }
 //           />
 //           <Route
@@ -372,11 +342,15 @@ export const MainView = () => {
 //               <>
 //                 {!user ? (
 //                   <Navigate to="/login" replace />
+//                 ) : movies.length === 0 ? (
+//                   <Col>The list is empty!</Col>
 //                 ) : (
 //                   <Col md={8}>
-//                     <MovieView movies={movies} />
+//                     {/* <MovieView movies={movies} /> */}
+//                     <MovieView user={user} token={token} setUser={setUser} />
 //                   </Col>
 //                 )}
+
 //               </>
 //             }
 //           />
@@ -384,215 +358,22 @@ export const MainView = () => {
 //             path="/"
 //             element={
 //               <>
-//                 {!user ? (
-//                   <Navigate to="/login" replace />
-//                 ) : movies.length === 0 ? (
-//                   <Col>There are no movies in the list!</Col>
-//                 ) : (
-//                   <>
-//                     {filteredMovies.map((movie) => (
-//                       <Col className="mb-5" key={movie._id} md={3}>
-//                         <MovieCard movie={movie} user={user} setUser={setUser} token={token} />
-//                       </Col>
-//                     ))}
-//                   </>
-//                 )}
-//               </>
-//             }
-//           />
-//           <Route
-//             path="/profile"
-//             element={
-//               <>
-//                 {!user ? (
-//                   <Navigate to="/login" replace />
-//                 ) : (
-//                   <Col>
-//                     <ProfileView
-//                       user={user}
-//                       movies={movies}
-//                       removeFav={removeFav}
-//                       addFav={addFav}
-//                       setUser={setUser}
-//                     />
-//                   </Col>
-//                 )}
-//               </>
-//             }
-//           />
-//         </Routes>
-//       </Row>
-//     </BrowserRouter>
-//   );
-// };
+//                 {!user ?
 
-
-// import { useEffect, useState } from "react";
-// import { MovieCard } from "../movie-card/movie-card";
-// import { MovieView } from "../movie-view/movie-view";
-// import { SignupView } from "../signup-view/signup-view";
-// import { LoginView } from "../login-view/login-view";
-// import { NavigationBar } from "../navigation-bar/navigation-bar";
-// import { ProfileView } from "../profile-view/profile-view";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
-// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-// import "./main-view.scss";
-// import { useSelector, useDispatch } from "react-redux";
-// import { setMovies } from "../../redux/reducers/movies";
-// import { setUser } from "../../redux/reducers/user";
-
-
-
-
-// export const MainView = () => {
-//   const movies = useSelector((state) => state.movies.list);
-//   const { user, token } = useSelector((state) => state.user);
-//   const storedUser = JSON.parse(localStorage.getItem("user"));
-//   const storedToken = localStorage.getItem("token");
-//   // const [user, setUser] = useState(storedUser ? storedUser : null);
-//   // const [token, setToken] = useState(storedToken ? storedToken : null);
-//   // const [movies, setMovies] = useState([]);
-//   const filter = useSelector((state) => state.movies.filter).trim().toLowerCase();
-//   const filteredMovies = movies.filter((movie) => movie.title.toLowerCase().includes(filter));
-
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     if (storedUser && storedToken) {
-//       dispatch(setUser({ user: storedUser, token: storedToken }));
-//     }
-//   }, [dispatch]);
-
-//   // fetch data from Api
-//   useEffect(() => {
-//     if (!token) {
-//       return;
-//     }
-//     fetch("https://awolf-movies-app.onrender.com/movies", {
-//       headers: { Authorization: `Bearer ${token}` }
-//     })
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setMovies(data);
-//       });
-//   }, [token]);
-
-//   const addFav = (_id) => {
-//     fetch(`https://awolf-movies-app.onrender.com/users/${user.Username}/movies/${movie._id}`, {
-//       method: "POST",
-//       headers: {
-//         Authorization: `Bearer ${token}`
-//       }
-//     }).then((response) => {
-//       if (response.ok) {
-//         return response.json();
-//       } else {
-//         alert("Failed to add movie");
-//       }
-//     }).then((user) => {
-//       if (user) {
-//         localStorage.setItem("user", JSON.stringify(user));
-//         setUser(user);
-//       }
-//     }).catch(error => {
-//       console.error("Error: ", error);
-//     });
-//   };
-
-//   const removeFav = (_id) => {
-//     fetch(`https://awolf-movies-app.onrender.com/users/${user.Username}/movies/${movie._id}`, {
-//       method: "DELETE",
-//       headers: {
-//         Authorization: `Bearer ${token}`
-//       }
-//     }).then((response) => {
-//       if (response.ok) {
-//         return response.json();
-//       } else {
-//         alert("Failed to remove movie")
-//       }
-//     }).then((user) => {
-//       if (user) {
-//         localStorage.setItem("user", JSON.stringify(user));
-//         setUser(user);
-//       }
-//     }).catch(error => {
-//       console.error("Error: ", error);
-//     });
-//   };
-
-//   return (
-//     <BrowserRouter>
-//       <NavigationBar
-//         user={user}
-//         onLoggedOut={() => {
-//           setUser(null);
-//           localStorage.removeItem("token");
-//           localStorage.removeItem("user");
-//         }}
-//       />
-//       <Row className="main-container d-flex justify-content-center">
-//         <Routes>
-//           <Route
-//             path="/signup"
-//             element={
-//               <>
-//                 {!user ? (
-//                   <Navigate to="/" />
-//                 ) : (
-//                   <Col md={5}>
-//                     <SignupView />
-//                   </Col>
-//                 )}
-//               </>
-//             }
-//           />
-//           <Route
-//             path="/login"
-//             element={
-//               <>
-//                 {user ? (
-//                   <Navigate to="/" />
-//                 ) : (
-//                   <Col md={5}>
-//                     <LoginView onLoggedIn={(user) => setUser(user)} />
-//                   </Col>
-//                 )}
-//               </>
-//             }
-//           />
-//           <Route
-//             path="/movies/:movieId"
-//             element={
-//               <>
-//                 {!user ? (
-//                   <Navigate to="/login" replace />
-//                 ) : (
-//                   <Col md={8}>
-//                     <MovieView movies={movies} />
-//                   </Col>
-//                 )}
-//               </>
-//             }
-//           />
-//           <Route
-//             path="/"
-//             element={
-//               <>
-//                 {!user ? (
-//                   <Navigate to="/login" replace />
-//                 ) : movies.length === 0 ? (
-//                   <Col>There are no movies in the list!</Col>
-//                 ) : (
-//                   <>
-//                     {filteredMovies.map((movie) => (
-//                       <Col className="mb-5" key={movie._id} md={3}>
-//                         <MovieCard movie={movie} user={user} setUser={setUser} token={token} />
-//                       </Col>
-//                     ))}
-//                   </>
-//                 )}
+//                   (
+//                     <Navigate to="/login" replace />
+//                   ) : movies.length === 0 ? (
+//                     <Col>There are no movies in the list!</Col>
+//                   ) : (
+//                     <>
+//                       {filteredMovies.map((movie) => (
+//                         <Col className="mb-5" key={movie._id} md={3}>
+//                           <MovieCard movie={movie} user={user} setUser={setUser} token={token} />
+//                         </Col>
+//                       ))}
+//                     </>
+//                   )
+//                 }
 //               </>
 //             }
 //           />
